4e48b58fafecd305282774e5e2a79c5a56ede79b
goodjwon <firstman9@protonmail.com>
2022-08-28 22:16:20 +0900

    ui added

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Todo from './Todo'
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos, setTotods, filterReadTodos}) => {
   return (
 	<div className="todo-container">
 		<ul className="todo-list">
 			{todos.map(todo => (
-				<Todo text={todo.text} key={todo.id}/>
+				<Todo 
+				setTotods={setTotods}
+				todos={todos}
+				todo={todo}
+				text={todo.text} 
+				key={todo.id}/>
 			))}
 		</ul>
 	</div>
