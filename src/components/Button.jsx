7294454b74a6d97e6f3987bb7588238cd42b12cc
goodjwon <firstman9@protonmail.com>
2022-09-02 16:35:28 +0900

    copilot 기능 테스트

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,16 +4,20 @@ import styled from 'styled-components'
 const ButtonStyle = styled.button`
         background: transparent;
         border-radius: 3px;
+        background-color: black;
+        color: white;
         border: 2px solid palevioletred;
         color: palevioletred;
         margin: 0 1em;
         padding: 0.25em 1em;
+        cursor: pointer;
         `
 
 const Button = () => {
+
     
   return (
-    <ButtonStyle>Button</ButtonStyle>
+    <ButtonStyle>Buttonss</ButtonStyle>
   )
 }
 
